7e79cee90eb214e16530a9b41cc1af1a52008f0d
Kyle Roberts <kylerobertsdev@gmail.com>
2014-12-15 19:48:21 -0500

    Initial Commit

---
+++ I2CTest/I2CTest.cpp
@@ -0,0 +1,17 @@
+/*
+ * I2CTest.cpp
+ *
+ * Created: 12/15/2014 7:38:23 PM
+ *  Author: Kyle
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
